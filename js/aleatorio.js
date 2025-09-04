7854db823689239ba5622d47c8d9ea55ce612f1b
MateusAlbano1 <mateus.philippus@escola.pr.gov.br>
2025-09-04 12:05:11 -0300

    .

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
+const nomes = ["Wesley", "Emerson", "Osvaldo", "Rose", "Virginia", "Lucy"];
 
+function aleatorio(lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista(posicao);
+}
+
+const nome = aleatorio(nome)
